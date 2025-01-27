1d824a4d6ad2bc66282b716305d8c79586d07947
Mhmtugr <m.ugur_10@hotmail.com>
2025-01-27 17:03:22 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 // Buraya, backend'in tam URL'sini yazacaksın. Lokal test için genelde:
 // const BASE_URL = "http://localhost:5000";
-// Deploy ettiğinde Render adresi gibi bir şey olabilir:
-// const BASE_URL = "https://mehmet-endustriyeltakip-backend.onrender.com"
+// Deploy ettiğinde PythonAnywhere adresi gibi bir şey olabilir:
+const BASE_URL = "https://username.pythonanywhere.com";
 
-const BASE_URL = "http://localhost:5000";
+// Gerçek kullanıcının PythonAnywhere kullanıcı adını girin
+const BASE_URL = "https://Mhmtugr.pythonanywhere.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
