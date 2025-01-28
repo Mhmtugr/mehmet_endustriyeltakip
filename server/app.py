8f75cefa1307dd3fc6db7875dc8607c8fe6bd77e
Mhmtugr <m.ugur_10@hotmail.com>
2025-01-28 17:09:11 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,26 @@
-from server import app
+import sys
+import os
+
+from server.config import Config  # <-- Doğru import yolu!
+from flask import Flask
+
+from server.extensions import init_extensions, db, api  # <-- Doğru import yolu!
+from server.models import *  # <-- Doğru import yolu!
+from server.routes import register_routes  # <-- Doğru import yolu!
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    init_extensions(app)
+    register_routes(api)
+    
+    with app.app_context():
+        db.create_all()  # İlk çalıştırmada tabloları oluşturur
+        
+    return app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
