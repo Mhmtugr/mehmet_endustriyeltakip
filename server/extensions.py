4d4cbc737779d5db51f0192988577a3903a1229e
Mhmtugr <m.ugur_10@hotmail.com>
2025-01-31 08:35:35 +0300

    Update extensions.py

--- server/extensions.py
+++ server/extensions.py
@@ -1,11 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_restful import Api
 
 db = SQLAlchemy()
-api = Api()
 
 def init_extensions(app):
     db.init_app(app)
     CORS(app)
-    api.init_app(app)
