c8954d574f9802daeb342d021eec73c0db720021
Mhmtugr <m.ugur_10@hotmail.com>
2025-01-31 13:02:57 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -7,6 +7,27 @@ from server.extensions import db, init_extensions
 from server.models import *  # Tüm modelleri içe aktar
 from server.routes import register_routes
 
+
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  # Tüm istekler için CORS aç
+    api = Api(app)
+    register_routes(api)
+    app.config.from_object(Config)
+
+    # Extensions'ları başlat
+    init_extensions(app)
+
+    # Veritabanı tablolarını oluştur
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
