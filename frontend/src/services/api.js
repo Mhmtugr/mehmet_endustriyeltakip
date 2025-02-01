1881d9f6a6650382bd55f890277d2ce82cdaad13
Mhmtugr <m.ugur_10@hotmail.com>
2025-02-01 18:54:27 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
+// frontend/src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://mehmet-endustriyeltakip.onrender.com", // Backend URL'si
+  baseURL: "https://mehmet-endustriyeltakip.onrender.com", // Backend URL'nizi buraya girin
   headers: {
     "Content-Type": "application/json"
   }
