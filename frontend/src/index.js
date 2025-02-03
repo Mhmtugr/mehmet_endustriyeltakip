797748af4fd9162c142b95f88ea7df3afbfd1a32
Mhmtugr <m.ugur_10@hotmail.com>
2025-02-03 09:11:10 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
