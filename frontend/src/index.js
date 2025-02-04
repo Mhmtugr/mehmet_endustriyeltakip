b04a672295a8db9682e70b3ef63b2f4a4225628f
Mhmtugr <m.ugur_10@hotmail.com>
2025-02-04 08:23:58 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
 const container = document.getElementById('root');
 const root = createRoot(container);
